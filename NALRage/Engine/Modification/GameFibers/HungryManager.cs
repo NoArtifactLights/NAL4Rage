1af93a00f82a137b7ca97c7cabe0354855fb87c3
RelaperCrystal <EndermanLOTR@outlook.com>
2021-01-23 21:14:56 +0800

    :bug: Fixed a bug causing Hungry values being constantly reset

--- NALRage/Engine/Modification/GameFibers/HungryManager.cs
+++ NALRage/Engine/Modification/GameFibers/HungryManager.cs
@@ -1,4 +1,6 @@
-﻿using Rage;
+﻿using System.Drawing;
+using Rage;
+using Rage.Attributes;
 using RAGENativeUI.Elements;
 
 namespace NALRage.Engine.Modification.GameFibers
@@ -10,6 +12,18 @@ namespace NALRage.Engine.Modification.GameFibers
 
         private static TimerBarPool pool = new TimerBarPool();
         private static BarTimerBar hungryBar;
+
+        [ConsoleCommand]
+        public static void RefillHungry()
+        {
+            hungry = 10f;
+        }
+
+        [ConsoleCommand]
+        public static void GetHungryRawValue()
+        {
+            Game.Console.Print(hungry.ToString());
+        }
 
         internal static void Init()
         {
@@ -40,15 +54,27 @@ namespace NALRage.Engine.Modification.GameFibers
         internal static void Fiber()
         {
             GameFiber.Sleep(1500);
-            if (hungry < 10f) hungry = 10f;
+            if (hungry > 10f) hungry = 10f;
             
-            if(hungry <= 2.5f)
+            if (hungry <= 2.5f)
             {
                 Game.LocalPlayer.Character.Health--;
             }
 
-            float offset = Game.LocalPlayer.Character.IsSprinting ? 0.05f : 0.01f;
-            hungry -= offset;
+            float offset = 0.01f;
+            if (Game.LocalPlayer.Character.IsSprinting)
+            {
+                offset = 0.1f;
+                hungryBar.ForegroundColor = Color.Red;
+                hungryBar.BackgroundColor = Color.DarkRed;
+            }
+            else
+            {
+                hungryBar.ForegroundColor = Color.White;
+                hungryBar.BackgroundColor = Color.Gray;
+            }
+
+            hungry = hungry - offset;
 
             hungryBar.Percentage = hungry / 10;
             GameFiber.Yield();
